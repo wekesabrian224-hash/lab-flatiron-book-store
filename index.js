fad4b38446201815b64466651f74ef8a3f90a2d1
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-07-12 01:02:20 +0300

    workedd out and learnt the use of DOM in manipulation

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
 const bookStore = {
-    name: 'Flatbooks Technical Books',
-    books: [
-        {
-            id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
-        },
-        {
-            id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-        },
-        {
-            id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
-        },
-        {
-            id:5,
-            title: 'You Don’t Know JS',
-            author: 'Kyle Simpson',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-        },
-        {
-            id:6,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-            
-        }
-    ]
-}
+  name: "Flatbooks Technical Books",
+  books: [
+    {
+      id: 1,
+      title: "Eloquent JavaScript: A Modern Introduction to Programming",
+      author: "Marjin Haverbeke",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      id: 2,
+      title: "JavaScript & JQuery: Interactive Front-End Web Development",
+      author: "Jon Duckett",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 3,
+      title: "JavaScript: The Good Parts",
+      author: "Douglas Crockford",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      id: 4,
+      title: "JavaScript: The Definitive Guide",
+      author: "David Flanagan",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 5,
+      title: "You Don’t Know JS",
+      author: "Kyle Simpson",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 6,
+      title: "Cracking the Coding Interview",
+      author: "Gayle Laakmann McDowell",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg",
+    },
+  ],
+};
 
 // Write your code here!
 
+const bookStoreTitle = document.getElementById("header");
+const bookList = document.getElementById("book-list");
+
+bookStoreTitle.textContent = bookStore.name;
+
+bookStore.books.forEach(function (book) {
+  const bookContainer = document.createElement("li");
+  const bookTitle = document.createElement("h3");
+  const bookAuthor = document.createElement("p");
+  const bookImage = document.createElement("img");
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookImage.src = book.imageUrl;
+
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(bookImage);
+
+  bookList.appendChild(bookContainer);
+});
